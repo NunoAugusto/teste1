0210bb6e03feb3a71b29157c3fa5935f9c3335b5
Nuno Fernandes <nunoabf@gmail.com>
2012-09-14 01:01:09 +0100

    plus 14 do 9

--- app/controllers/pontinhos_controller.rb
+++ app/controllers/pontinhos_controller.rb
@@ -6,16 +6,31 @@ class PontinhosController < ApplicationController
 
   def index
     @pontinhos = Pontinho.all
-
   end
+
   def criaPonto(sandc,anumd,anump,aX,aY)
     if sandc==1 
       Pontinho.Create(:NumDesenho=>anumd,:NumPonto=>anump,:CX=>aX,:CY=>aY,) 
     end   
   end  
+  def create
+  end
+  
   def destroy
-      @pontinho = Pontinho.find(params[:id])
-      @pontinho.destroy
+    @pontinhos = Pontinho.all
+    @pontinhos.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(index) }
+      format.xml  { head :ok }
     end
-     
+  end
+  def guardalimpa(hda)
+    @pontinhos = Pontinho.all
+      #aqui deve estar o código para inserir na BD o desenho hda ou em hash ou em array
+    respond_to do |format|
+        format.html { redirect_to(index) 
+        format.xml  { head :ok }}
+    end
+  end  
 end
